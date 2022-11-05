3595e38787d70469255de3cfc6e24f68f61666e0
P1100s7 <mmanp277@gmail.com>
2022-11-05 16:54:10 -0400

    updates

---
+++ script.js
@@ -0,0 +1,24 @@
+//
+var currentDayElem = document.getElementById("current-day")
+var fiveDay = document.getElementById("five-day")
+
+// the strings for the api
+var apiKey = ""
+var apiUrl = "https://api.openWeatherMap.org"
+var searchHistory = []
+var localDateTime = new Date()
+
+
+function getUserLocation(city) {
+    console.log(city)
+}
+
+
+function getWeather(location) {
+    console.log(location)
+}
+
+
+function getForcast(location) {
+    console.log(location)
+}
